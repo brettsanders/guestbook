363dd772a45255f1b2de6d92201ccfd5138392d1
Brett Sanders <brettrsanders@gmail.com>
2015-01-12 00:05:52 -0500

    Add ability to add and persist messages

--- project.clj
+++ project.clj
@@ -2,9 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.6"]
-                 [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                [compojure "1.1.5"]
+                [hiccup "1.0.4"]
+                [ring-server "0.3.1"]
+                ;;JDBC dependencies
+                [org.clojure/java.jdbc "0.2.3"]
+                [org.xerial/sqlite-jdbc "3.7.2"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler guestbook.handler/app
          :init guestbook.handler/init
